4fa2344e82fe3d904db69a84e62d746c1999e33e
jesusYanezx01 <jesusmitesoro2004@gmail.com>
2022-11-16 16:59:21 -0500

    Se pueden registrar estudiantes en el apartado "Estudiantes"

---
+++ src/main/java/com/mycompany/projectsibies/dao/IDaoEstudiantes.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projectsibies.dao;
+
+import com.mycompany.projectsibies.modelos.Estudiantes;
+
+/**
+ *
+ * @author Jesus
+ */
+public interface IDaoEstudiantes {
+    public boolean registrarEstudiante(Estudiantes est);
+}
